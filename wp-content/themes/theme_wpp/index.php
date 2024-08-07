46b3d2055e736eae6e9384aac74e7d09d8bfeecb
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-07 21:38:59 +0600

    logo added

--- wp-content/themes/theme_wpp/index.php
+++ wp-content/themes/theme_wpp/index.php
@@ -1,14 +1,24 @@
-<?php?>
+<? php ?>
 <!DOCTYPE html>
-<html lang="<?php language_attributes();?>">
+<html lang="<?php language_attributes(); ?>">
+
 <head>
   <meta charset="<?php bloginfo('charset') ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head();?>
+  <?php wp_head(); ?>
 </head>
-<body <?php body_class() ?> >
-  <!-- The Content Will Be Published -->
-<?php  wp_footer() ?>
-</body>
-</html>
 
+<body <?php body_class() ?>>
+
+  <div id="header_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3"><a href=""><img src="" alt=""></a></div>
+        <div class="col-md-9"></div>
+      </div>
+    </div>
+  </div>
+  <?php wp_footer() ?>
+</body>
+
+</html>
