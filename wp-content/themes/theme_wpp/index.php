43e55c1591b78d6e9e1528d5db78875f82b96f13
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-07 19:15:25 +0600

    working with functions

---
+++ wp-content/themes/theme_wpp/index.php
@@ -0,0 +1,15 @@
+
+
+<?php    ?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes();?>">
+<head>
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
+
