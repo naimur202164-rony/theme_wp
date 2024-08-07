ca3834e2b12403a850ccd51a67e3cb0ca17e4317
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-07 23:01:26 +0600

    custom css added

--- wp-content/themes/theme_wpp/index.php
+++ wp-content/themes/theme_wpp/index.php
@@ -12,7 +12,37 @@
     <div class="container">
       <div class="row">
         <div class="col-md-3"><a href=""><img src="<?php echo get_theme_mod('naimur_logo') ?>" alt=""></a></div>
-        <div class="col-md-9"></div>
+        <div class="col-md-9">
+
+        <ul id="nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Media</a></li>
+          <li><a href="#">Download</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Service</a>
+            <ul>
+              <li><a href="">Dropdown Menu</a></li>
+              <li><a href="">Dropdown Menu</a></li>
+              <li><a href="">Dropdown Menu</a></li>
+              <li><a href="">Dropdown Menu</a>
+            
+              <ul>
+                <li><a href="">Dropdown menu 2</a></li>
+                <li><a href="">Dropdown menu 2</a></li>
+                <li><a href="">Dropdown menu 2</a></li>
+                <li><a href="">Dropdown menu 2</a></li>
+                <li><a href="">Dropdown menu 2</a></li>
+              </ul>
+            </li>
+              <li><a href="">Dropdown Menu</a></li>
+              <li><a href="">Dropdown Menu</a></li>
+            </ul>        
+        </li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+        </div>
       </div>
     </div>
   </div>
