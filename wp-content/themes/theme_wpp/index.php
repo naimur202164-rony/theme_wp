7bd1504f206d642250e67904f7fa58ea0cbc0af6
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-08 19:01:54 +0600

    working with header section

--- wp-content/themes/theme_wpp/index.php
+++ wp-content/themes/theme_wpp/index.php
@@ -14,7 +14,6 @@
         <div class="col-md-3"><a href=""><img src="<?php echo get_theme_mod('naimur_logo') ?>" alt=""></a></div>
         <div class="col-md-9">
           <?php wp_nav_menu(array('theme_location'=>'main_menu','menu_id'=>'nav')); ?>
-
         </div>
       </div>
     </div>
