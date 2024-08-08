10eba9b9c947b08c68a975ba3bc93fbb91bbaf80
MD Naimur Rahman <naimur202164@gmail.com>
2024-08-08 17:55:36 +0600

    all problem was fixed

--- wp-content/themes/theme_wpp/functions.php
+++ wp-content/themes/theme_wpp/functions.php
@@ -59,15 +59,29 @@ function naimur_customizar_register($wp_customize){
   ) ));
 
 
-// Menu Position Option
+ // Menu Position Option
   $wp_customize->add_section('naimur_menu_option', array(
     'title' => __('Menu Position Option', 'naimurTheme'),
     'description' => 'If you interested to change your menu position you can do it.'
   ));
 
-  $wp_customize->add_section('naimur_menu_position', array(
-    'default' => 'right_menu'
+  $wp_customize->add_setting('naimur_menu_position', array(
+    'default' => 'right_menu',
   ));
+
+  $wp_customize-> add_control('naimur_menu_position', array(
+    'label' => 'Menu Position',
+    'description' => 'Select your menu position',
+    'setting' => 'naimur_menu_position',
+    'section' => 'naimur_menu_option',
+    'type' => 'radio',
+    'choices' => array(
+      'left_menu' => 'Left Menu',
+      'right_menu' => 'Right Menu',
+      'center_menu' => 'Center Menu',
+    ),
+  ));
+
 }
 
 
